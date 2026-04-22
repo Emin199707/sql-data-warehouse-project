63a66cb7a698729135278030cf5df328b460842d
Emin199707 <149092823+Emin199707@users.noreply.github.com>
2026-04-22 11:45:28 +0400

    Update stored_procedure_bulk_insert_bronze_layer.sql

--- scripts/stored_procedure_bulk_insert_bronze_layer.sql
+++ scripts/stored_procedure_bulk_insert_bronze_layer.sql
@@ -1,3 +1,4 @@
+-- Change [Path] placeholder and add paths of CSV files
 USE DataWarehouse;
 GO
 
@@ -7,7 +8,7 @@ BEGIN
 	TRUNCATE TABLE bronze.crm_cust_info;
 	
 	BULK INSERT bronze.crm_cust_info
-	FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	FROM '[Path]\cust_info.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -19,7 +20,7 @@ BEGIN
 	TRUNCATE TABLE bronze.crm_prd_info;
 	
 	BULK INSERT bronze.crm_prd_info
-	FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	FROM '[Path]\prd_info.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -32,7 +33,7 @@ BEGIN
 	TRUNCATE TABLE bronze.crm_sales_details;
 	
 	BULK INSERT bronze.crm_sales_details
-	FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	FROM '[Path]\sales_details.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -46,7 +47,7 @@ BEGIN
 	TRUNCATE TABLE bronze.erp_cust_az12;
 	
 	BULK INSERT bronze.erp_cust_az12
-	FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	FROM '[Path]\CUST_AZ12.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -59,7 +60,7 @@ BEGIN
 	TRUNCATE TABLE bronze.erp_loc_a101;
 	
 	BULK INSERT bronze.erp_loc_a101
-	FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+	FROM '[Path]\LOC_A101.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -72,7 +73,7 @@ BEGIN
 	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 	
 	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	FROM '[Path]\PX_CAT_G1V2.csv'
 	WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
