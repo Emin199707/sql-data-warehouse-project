17703b8453e243271cba6a0558f1adff5c5d85a4
Emin199707 <149092823+Emin199707@users.noreply.github.com>
2026-04-22 15:26:10 +0400

    Update stored_procedure_bulk_insert_bronze_layer.sql

--- scripts/stored_procedure_bulk_insert_bronze_layer.sql
+++ scripts/stored_procedure_bulk_insert_bronze_layer.sql
@@ -1,85 +1,159 @@
--- Change [Path] placeholder and add paths of CSV files
+/*
+==============================================================
+Create bronze.load_bronze Stored Procedure
+==============================================================
+
+Script Purpose:
+    Create or Drop and recreate 'bronze.load_brobze' SP if it already exist. This Procedure will bulk insert whole data in specified CSV files everytime it is executed.
+	Change [Path] placeholder in the FROM statements  after downloading CSV files from "Datasets" folder into a specific locations and add them to the [Path] placeholder.
+WARNING:
+    Running this script will first Truncate all data in the tables and reinsert them everytime it is executed. So proceed with catuious if you bulk insert new data from CSV files.
+
+*/
+
 USE DataWarehouse;
 GO
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '=============================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '=============================';
 
-	TRUNCATE TABLE bronze.crm_cust_info;
-	
-	BULK INSERT bronze.crm_cust_info
-	FROM '[Path]\cust_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
+		PRINT '-----------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '-----------------------------';
 
-	SELECT COUNT(*) crm_cust_info FROM bronze.crm_cust_info;
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+	
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> ------------';
 
-	TRUNCATE TABLE bronze.crm_prd_info;
-	
-	BULK INSERT bronze.crm_prd_info
-	FROM '[Path]\prd_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
-	
-	SELECT COUNT(*) crm_prd_info FROM bronze.crm_prd_info;
-	
+		SELECT COUNT(*) crm_cust_info FROM bronze.crm_cust_info;
 
-	TRUNCATE TABLE bronze.crm_sales_details;
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_prd_info';
+		TRUNCATE TABLE bronze.crm_prd_info;
+		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 	
-	BULK INSERT bronze.crm_sales_details
-	FROM '[Path]\sales_details.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> ------------';
 	
-	SELECT COUNT(*) crm_sales_details FROM bronze.crm_sales_details;
+		SELECT COUNT(*) crm_prd_info FROM bronze.crm_prd_info;
+	
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
+		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+	
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> ------------';
+	
+		SELECT COUNT(*) crm_sales_details FROM bronze.crm_sales_details;
 
 	
+		PRINT '-----------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '-----------------------------';
 
-	TRUNCATE TABLE bronze.erp_cust_az12;
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 	
-	BULK INSERT bronze.erp_cust_az12
-	FROM '[Path]\CUST_AZ12.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> ------------';
 	
-	SELECT COUNT(*) erp_cust_az12 FROM bronze.erp_cust_az12;
+		SELECT COUNT(*) erp_cust_az12 FROM bronze.erp_cust_az12;
 	
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_loc_a101';
+		TRUNCATE TABLE bronze.erp_loc_a101;
+		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+	
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> ------------';
+	
+		SELECT COUNT(*) erp_loc_a101 FROM bronze.erp_loc_a101;
+	
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+	
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\Nebim.Test\Documents\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> ------------';
 
-	TRUNCATE TABLE bronze.erp_loc_a101;
+		SET @batch_end_time = GETDATE();
+		PRINT '==================================================';
+		PRINT 'Loading Bronze Layer is Completed';
+		PRINT '		-	Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '==================================================';
 	
-	BULK INSERT bronze.erp_loc_a101
-	FROM '[Path]\LOC_A101.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
+		SELECT COUNT(*)  px_cat_g1v2 FROM bronze.erp_px_cat_g1v2;
+	END TRY
+	BEGIN CATCH
+		PRINT '==================================================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '==================================================';
+	END CATCH
 	
-	SELECT COUNT(*) erp_loc_a101 FROM bronze.erp_loc_a101;
-	
-
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-	
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM '[Path]\PX_CAT_G1V2.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
-	
-	SELECT COUNT(*)  px_cat_g1v2 FROM bronze.erp_px_cat_g1v2;
 	
 END
