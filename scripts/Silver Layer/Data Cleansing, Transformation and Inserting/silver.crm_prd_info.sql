193820604660b8a6c2fc58663e09dfc781f388ac
Emin199707 <149092823+Emin199707@users.noreply.github.com>
2026-04-28 18:30:10 +0400

    Update and rename bronze.crm_prd_info table checking.sql to silver.crm_prd_info.sql

--- scripts/Silver Layer/Data Cleansing, Transformation and Inserting/silver.crm_prd_info.sql
+++ scripts/Silver Layer/Data Cleansing, Transformation and Inserting/silver.crm_prd_info.sql
@@ -20,8 +20,8 @@ CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
 		 ELSE 'n/a'
 */
 
-SELECT DISTINCT prd_lines
-FROM bronze.crm_prd_info
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info;
   
 -- Checking if our bronze schema tables have data row where start date is later than end date.
 SELECT * FROM bronze.crm_prd_info
@@ -69,3 +69,61 @@ CREATE TABLE silver.crm_prd_info (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+-- Finally we will insert data to the silver layer table:
+
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT 
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') as cat_id,
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+	prd_nm,
+	ISNULL(prd_cost, 0) AS prd_cost,
+	CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+		 WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+		 WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'other Sales'
+		 WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+		 ELSE 'n/a'
+	END AS prd_line,
+	CAST (prd_start_dt AS DATE) AS prd_start_dt,
+	CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info;
+
+-- Quality check scripts for silver layer table after Inserting:
+SELECT prd_id, COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- ------------------------------------
+
+SELECt prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- ------------------------------------
+
+
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- ------------------------------------
+
+
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info;
+
+-- -----------------------------------
+
+
+SELECT * FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
