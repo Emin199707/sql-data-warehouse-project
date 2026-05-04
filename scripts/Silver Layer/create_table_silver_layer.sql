0d80acd266b470734203a27c8e3849abc89445be
Emin199707 <149092823+Emin199707@users.noreply.github.com>
2026-05-04 18:18:31 +0400

    Update create_table_silver_layer.sql

--- scripts/Silver Layer/create_table_silver_layer.sql
+++ scripts/Silver Layer/create_table_silver_layer.sql
@@ -60,8 +60,7 @@ IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
 	cid NVARCHAR(50),
-	bdate DATE,
-	gen NVARCHAR(50),
+	cntry NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
